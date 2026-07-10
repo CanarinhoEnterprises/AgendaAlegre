0fa892ee64773ee8264a8d6c7a975f6eb24b5871
Alexandre Rangel <familiagamer1054@gmail.com>
2026-07-10 12:02:03 -0300

    Adiciona projeto spring boot

---
+++ backend/reserva-espacos/src/main/java/br/ufes/reserva_espacos/ReservaEspacosApplication.java
@@ -0,0 +1,13 @@
+package br.ufes.reserva_espacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaEspacosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaEspacosApplication.class, args);
+	}
+
+}
---
+++ backend/reserva-espacos/src/test/java/br/ufes/reserva_espacos/ReservaEspacosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufes.reserva_espacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaEspacosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
