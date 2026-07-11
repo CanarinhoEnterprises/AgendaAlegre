f0d678fa235e12377bb23da04e65fdd393601c26
Adrian Vieira Cunha <adrianvieiracunha@gmail.com>
2026-07-10 21:49:57 -0300

    Minor changes.2

---
+++ backend/reserva-espacos/src/main/java/br/ufes/reserva_espacos/controller/AdministradorController.java
@@ -0,0 +1,35 @@
+package br.ufes.reserva_espacos.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import br.ufes.reserva_espacos.dto.CadastroAdministradorDTO;
+import br.ufes.reserva_espacos.entity.Administrador;
+import br.ufes.reserva_espacos.service.AdministradorService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+@RequestMapping("/administradores")
+public class AdministradorController {
+
+    private final AdministradorService administradorService;
+
+
+    public AdministradorController(
+            AdministradorService administradorService) {
+
+        this.administradorService = administradorService;
+    }
+
+
+    @PostMapping
+    public Administrador cadastrar(
+            @RequestBody CadastroAdministradorDTO dto) {
+
+        return administradorService.cadastrar(dto);
+    }
+
+}
