269a4017c75fe292dd0f38199c901d98452b6c76
Talleba <tallesbazani@live.com>
2026-07-14 13:26:19 -0300

    seed atualizada

--- database/seed_espacos.sql
+++ database/seed_espacos.sql
@@ -1,5 +1,45 @@
+-- Função pra apagar os dados de todas as tabelas
+
+DO $$ 
+DECLARE 
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
+        EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' RESTART IDENTITY CASCADE;';
+    END LOOP;
+END $$;
+
+-- Funções para ver os dados da tabela
+
+SELECT * FROM usuario;
+SELECT * FROM solicitante;
+SELECT * FROM administrador;
+SELECT * FROM cidade;
+SELECT * FROM endereco;
+SELECT * FROM espaco;
+SELECT * FROM tipoespaco;
+SELECT * FROM analiseReserva;
+SELECT * FROM avaliacao;
+SELECT * FROM reserva;
+
+-- Add usuario
+
+INSERT INTO usuario(
+	nome, telefone, email, senha)
+	VALUES ('Talles', 28999097419, 'tallesAdmin@email.com', 123);
+
+-- Add usuario Admin
+
+INSERT INTO administrador(
+	idusuario, cpf)
+	VALUES (?, ?);
+
+-- Add Cidade
+
 INSERT INTO Cidade (UF, nome) VALUES
 ('ES', 'Alegre');
+
+-- Add Tipos de espaço
 
 INSERT INTO TipoEspaco (nome, descricao) VALUES
 ('Quadra esportiva', 'Espaço destinado a atividades esportivas e recreativas.'),
@@ -9,6 +49,8 @@ INSERT INTO TipoEspaco (nome, descricao) VALUES
 ('Auditório', 'Espaço para apresentações, reuniões e atividades culturais.'),
 ('Centro cultural', 'Espaço destinado a atividades artísticas e culturais.');
 
+-- Add Endereço
+
 INSERT INTO Endereco (idCidade, logradouro, bairro, cep, num, comp, referencia) VALUES
 (1, 'Avenida Jerônimo Monteiro', 'Centro', '29500000', 100, NULL, 'Próximo à prefeitura'),
 (1, 'Rua Boa Vista', 'Boa Vista', '29500001', 45, NULL, 'Ao lado da escola municipal'),
@@ -17,6 +59,8 @@ INSERT INTO Endereco (idCidade, logradouro, bairro, cep, num, comp, referencia) 
 (1, 'Rodovia ES-482', 'Distrito de Celina', '29500003', NULL, NULL, 'Área central do distrito'),
 (1, 'Rua Sete de Setembro', 'Centro', '29500000', 520, NULL, 'Próximo à biblioteca');
 
+-- Add Espaço
+
 INSERT INTO Espaco (idEndereco, idTipoEspaco, capacidade, nome, descricao, urlCapa, status) VALUES
 (1, 1, 24, 'Quadra Poliesportiva Central', 'Quadra para futsal, vôlei e atividades comunitárias.', 'https://www.pmbsf.es.gov.br/uploads/files/quadra-do-vargem-alegre.jpg', 'ATIVO'),
 (2, 1, 18, 'Quadra do Bairro Boa Vista', 'Quadra de uso comunitário do bairro Boa Vista.', 'https://www.vitoria.es.gov.br/recursos/imagens/banco/2023/03/06/110731/normal@2x.jpg', 'INTERDITADO'),
@@ -24,7 +68,9 @@ INSERT INTO Espaco (idEndereco, idTipoEspaco, capacidade, nome, descricao, urlCa
 (4, 4, 150, 'Salão Comunitário São José', 'Salão para festas e reuniões da comunidade.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWo0HUdFQRQNAqxaYpEJSfAyhUzhgia-wQ1Mry0B2J4zCQ7dMGgDyAn3Nt&s=10', 'INATIVO'),
 (5, 4, 180, 'Salão de Festas do Distrito', 'Salão comunitário do distrito de Celina.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDiTrxl5IAcPGvITz6vKhsxJL0zx0xnB_YT8WqvonwMMqQb5jkFdGn-WMq&s=10', 'ATIVO'),
 (6, 5, 220, 'Auditório Municipal', 'Auditório principal para eventos institucionais.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmHPwTfbkwAljq-lG-h73E4ikC6nkn08fKQWY1F6I4xoOAsySut2v68A&s=10', 'ATIVO'),
-(6, 6, 160, 'Centro Cultural de Alegre', 'Espaço voltado para oficinas e apresentações culturais.', 'https://descubraoespiritosanto.es.gov.br/Media/DescubraOEspiritoSanto/_Profiles/7996852b/34b24092/Teatro%20Municipal%20de%20Alegre%20(3).jpg?v=639005206838552300, 'INATIVO');
+(6, 6, 160, 'Centro Cultural de Alegre', 'Espaço voltado para oficinas e apresentações culturais.', 'https://descubraoespiritosanto.es.gov.br/Media/DescubraOEspiritoSanto/_Profiles/7996852b/34b24092/Teatro%20Municipal%20de%20Alegre%20(3).jpg?v=639005206838552300', 'INATIVO');
+
+-- Add Reserva já confirmada 
 
 INSERT INTO Reserva (
     idSolicitante, idEspaco, horaInicio, dtUso, horaFim,
@@ -32,8 +78,8 @@ INSERT INTO Reserva (
     qtdPessoas, status
 )
 VALUES (
-    4,                                  -- idSolicitante
-    9,                                 -- idEspaco
+    1,                                  -- idSolicitante
+    1,                                 -- idEspaco
     '10:50:00',                         -- horaInicio
     '2025-07-12',                       -- dtUso
     '10:56:00',                         -- horaFim
@@ -45,6 +91,8 @@ VALUES (
     'CONFIRMADA'                        -- status
 );
 
+-- Add avaliação pré preenchida para reserva confirmada
+
 INSERT INTO Avaliacao (
     idReserva, nota, comentario, dtAvaliacao
 )
@@ -53,4 +101,25 @@ VALUES (
     5,                                                   -- nota (1 a 5)
     'Espaço muito bem cuidado e de fácil acesso.',       -- comentario (até 1000 caracteres)
     CURRENT_DATE                                         -- dtAvaliacao
+);
+
+-- Add reserva já confirmada pt2
+
+INSERT INTO Reserva (
+    idSolicitante, idEspaco, horaInicio, dtUso, horaFim,
+    finalidade, dtSolicitacao, dtCancelamento, dtConfirmacao,
+    qtdPessoas, status
+)
+VALUES (
+    1,                                  -- idSolicitante
+    1,                                 -- idEspaco
+    '13:40:00',                         -- horaInicio
+    '2025-08-06',                       -- dtUso
+    '17:42:00',                         -- horaFim
+    'Campeonato de farmar aura',    -- finalidade
+    NOW(),                              -- dtSolicitacao
+    NULL,                               -- dtCancelamento
+    NULL,                               -- dtConfirmacao
+    30,                                 -- qtdPessoas
+    'CONFIRMADA'                        -- status
 );
